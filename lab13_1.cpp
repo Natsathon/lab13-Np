3ccc21074344dbc31fcbed2a2ebefcfa6073b217
_pooh <natsathon2018@gmail.com>
2024-12-31 15:46:38 +0700

    commit

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,42 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[], int N, double B[]) {
+    double sum = 0;
+    for (int i = 0; i < N; ++i) {
+        sum += A[i];
+    }
+    B[0] = sum / N;
+
+    double variance = 0;
+    for (int i = 0; i < N; ++i) {
+        variance += (A[i] - B[0]) * (A[i] - B[0]);
+    }
+    B[1] = sqrt(variance / N);
+
+    double product = 1;
+    for (int i = 0; i < N; ++i) {
+        product *= A[i];
+    }
+    B[2] = pow(product, 1.0 / N);
+
+    double harmonicSum = 0;
+    for (int i = 0; i < N; ++i) {
+        harmonicSum += 1.0 / A[i];
+    }
+    B[3] = N / harmonicSum;
+
+    B[4] = A[0];
+    for (int i = 1; i < N; ++i) {
+        if (A[i] > B[4]) {
+            B[4] = A[i];
+        }
+    }
+
+    B[5] = A[0];
+    for (int i = 1; i < N; ++i) {
+        if (A[i] < B[5]) {
+            B[5] = A[i];
+        }
+    }
+}
